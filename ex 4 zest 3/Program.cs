f62cd8b3f29599a6456e582f044d65c1ba4b2559
barbara.jamroga <barbara.jamroga@microsoft.wsei.edu.pl>
2019-04-11 09:31:31 +0200

    commit silnia

--- ex 4 zest 3/Program.cs
+++ ex 4 zest 3/Program.cs
@@ -10,6 +10,21 @@ namespace ex_4_zest_3
     {
         static void Main(string[] args)
         {
+            int n = 20;
+            long fctrl = Factorial(n);
+
+            Console.WriteLine("The factorial of {0} equals {1}.", n, fctrl);
+
+            Console.ReadKey();
+        }
+        /***************************************************/
+        static long Factorial(int n)
+        {
+            //0! = 1 nad 1! = 1
+            if (n <= 1) return 1;
+            //in other cases, call the function recursively with the previous
+            // number as its argument (n-1) and multiply the outcume by n 
+            return n * Factorial(n - 1);
         }
     }
 }
